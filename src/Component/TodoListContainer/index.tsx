40599cac2cb526da45d53f0c48f0605374eb18a3
山口圭太 <yamaguchikeita@yamaguchikeitanoMacBook-Pro.local>
2019-12-23 16:43:46 +0900

    add Circle State

--- src/Component/TodoListContainer/index.tsx
+++ src/Component/TodoListContainer/index.tsx
@@ -14,27 +14,28 @@ const Label = Styled.Text`
     font-weight: 600;
     font-size: 20px;
     margin-vertical: 20px;
+    color: ${(props: {isCompleted: boolean;}) => props.isCompleted ? '#bbb' : '#353839'};
+    text-decoration-line: ${(props: {isCompleted: boolean;}) => props.isCompleted ? 'line-through' : 'none'};
 `;
 
 const Circle = Styled.View`
     width: 30px;
     height: 30px;
-    border-color: red;
+    border-color: ${(props: {isCompleted: boolean;}) => props.isCompleted ? '#bbb' : '#F23567'};
     border-radius: 15px
     border-width: 3px
     margin-right: 20px;
 `;
 
-// const {width, height} = Dimensions.get('window');
-
 const TodoListCntainer = () => {
   const [isCompleted, setIsCompleted] = useState<boolean>(false);
   return (
         <Container>
-          <TouchableOpacity>
-            <Circle />
+          <TouchableOpacity 
+            onPress={() => setIsCompleted(!isCompleted)}>
+            <Circle isCompleted={isCompleted} />
           </TouchableOpacity>
-          <Label>Todo</Label>
+          <Label isCompleted={isCompleted}>Todo</Label>
         </Container>
     );
 };
